d8171eb0b80b8962bbf633a08f3ba2f0ff9c78d4
Andrei Blinov <dus.blinov@gmail.com>
2019-08-02 18:24:36 +0300

    Update production.js

--- config/production.js
+++ config/production.js
@@ -12,28 +12,28 @@ var settings = {
 		host: 'localhost',
 		user: 'root',
 		password: 'securepass',
-		database: 'meetpa',
+		database: '',
 		multipleStatements: true,
 		connectionLimit: 100,
-		charset : 'utf8mb4'
+		charset : ''
 	},
 	bodyLimit: '100kb',
-	adminEmail: 'dus.blinov@gmail.com',
-	emailFrom: 'noreply@meetpa.co',
+	adminEmail: '',
+	emailFrom: '',
 	mailgun: {
-		apiKey: 'key-e6e4cfd91ba4c399b3a714b416fe6979',
-		domain: 'mg.meetpa.co'
+		apiKey: '',
+		domain: ''
 	},
-	cryptAlgorithm: 'aes-256-ctr',
-	cryptKey: 'DWw2uRhh3jBk3hMT6Cm8vP3CESBIxQrx',
-	secret: 'uIxQT6PrxCmB28v',
-	salt: 'mC$2bh4ZOa$1ENMOiXaNvkXN9B0$w',
+	cryptAlgorithm: '',
+	cryptKey: '',
+	secret: '',
+	salt: '',
 	env: process.env.NODE_ENV,
 	braintree: {
 		environment: 'Sandbox',
-		merchantId: "p3jtcfh6khbnxjpc",
-		publicKey: "nkngfkhtpd5kpprx",
-		privateKey: "cae542c12a4b05501ddff17edceb8588"
+		merchantId: "",
+		publicKey: "",
+		privateKey: ""
 	}
 }
 module.exports = settings;
